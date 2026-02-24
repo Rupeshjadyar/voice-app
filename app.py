09162f3fd21334ba1325485e00045edbcd668099
Rupeshjadyar <jadyarrupesh20@gmail.com>
2026-02-25 00:54:45 +0530

    final sitemap fix

--- app.py
+++ app.py
@@ -437,4 +437,4 @@ def test_redis():
         return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000) 
